5a7532ffc45e5164cdce7783fab4c9c7d93dd0b1
cristiane <cris_csr@yahoo.com.br>
2019-05-21 13:11:03 -0300

    first commit

---
+++ src/teste_conceitual/Alfa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste_conceitual;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Alfa {
+    
+    int a;
+    double aa;
+    
+    public void setA(int a){
+        this.a = a;
+    }
+    
+      public int getA(){
+        return this.a;
+    }
+    
+}
---
+++ src/teste_conceitual/Beta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste_conceitual;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Beta {
+    int b;
+    double bb;
+    
+    public void setB(int b){
+        this.b = b;
+    }
+    
+      public int getB(){
+        return this.b;
+    }
+}
